8d8311d44a7bc51173a3b6f1defecfb35394fbcd
hubharsha <91658744+hubharsha@users.noreply.github.com>
2022-02-06 23:30:36 +0530

    Add files via upload

---
+++ Vehicle.java
@@ -0,0 +1,44 @@
+package car;
+
+public class Vehicle {
+    private String Number;
+    private String Name;
+    private String Type;
+    private String Mobilenumber;
+
+    public Vehicle() {
+        this.Name = Name;
+        this.Number = Number;
+        this.Type = Type;
+        this.Type = Mobilenumber;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+    public void setNumber(String Number) {
+        this.Number = Number;
+    }
+    public void setType(String Type) {
+        this.Type = Type;
+    }
+    public void setMobilenumber(String Mobilenumber) {
+        this.Type = Mobilenumber;
+    }
+
+    public String getName() {
+        return Name;
+    }
+    public String getNumber() {
+        return Number;
+    }
+    public String getType() {
+        return Type;
+    }
+    public String getMobilenumber() {
+        return Mobilenumber;
+    }
+    public String getIDnumber() {
+        return Number;
+    }
+}
